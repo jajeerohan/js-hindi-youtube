12d42fddcd8d6d88f80fde85c2b7f46c52065377
Rohan Jajee <95631987+jajeerohan@users.noreply.github.com>
2023-09-16 12:12:06 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234567
+let accountEmail = "jajee@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountDate;
+
+// accountId = 123
+accountEmail = "jajeegoogle.com"
+accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountDate])
