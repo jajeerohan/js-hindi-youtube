83012d10fd0cfb9a8926348b59f41252d748ef3b
Rohan Jajee <95631987+jajeerohan@users.noreply.github.com>
2023-09-16 12:57:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,58 @@
+//Converting to number
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log("valueInNumber = " ,valueInNumber) // 33
+
+let x = "33abc"
+console.log(typeof x)
+console.log(typeof(x))
+let y = Number(x)
+console.log(typeof y)
+console.log("y = ",y) // NaN
+
+let a = null
+console.log(typeof a)
+console.log(typeof(a))
+let b = Number(a)
+console.log(typeof b)
+console.log("b = ",b) // 0
+
+let c
+console.log(typeof c)
+console.log(typeof(c))
+let d = Number(c)
+console.log(typeof d)
+console.log("d = ", d) // Nan
+
+
+let e = false  //true = 1 , false = 0
+console.log(typeof e)
+console.log(typeof(e))
+let f = Number(e)
+console.log(typeof f)
+console.log("f = ",f) // 0
+
+// "33" = 33
+// "33abe" = Nan -> Not a Number
+// true = 1, false = 0
+
+// converting to Boolean
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) // true
+
+// "" => false
+// "redf" => true
+// 1 => true, 0=> false
+
+// conversion to String
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(typeof(stringSomeNumber))
+console.log(stringSomeNumber)
+
+
+
