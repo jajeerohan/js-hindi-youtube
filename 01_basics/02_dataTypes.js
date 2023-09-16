605392b14d54f3e4aea1c28fa7150c56b7ce3b48
Rohan Jajee <95631987+jajeerohan@users.noreply.github.com>
2023-09-16 12:23:50 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use stirct" //treats allthe JS code as newer version
+
+// alert(3+3) // doesn't work as we r using noode.js not browser
+
+let name = "rohan"
+let age = 10
+let isLoggedIn = false
+let gender;  // logs as undefined
+
+// number => range is 2 to power of 53 0r 54
+// bigint
+// String => "1qwef"
+// Boolean => true/false
+// null => standalone value
+// undefined => no value is assigned
+// Symbol => finding uniques components etc
+// Object
+
+console.log(typeof "hitest")
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) //object
+
+
+
+
