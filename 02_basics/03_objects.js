41a0c4535b61e7c34bbd7efa2aa485d90d80dc4f
Rohan Jajee <95631987+jajeerohan@users.noreply.github.com>
2023-09-17 18:09:48 +0000

    Objects in JS

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,23 @@
+// Singleton
+Object.create();
+
+// Object Literals 
+
+const jsUser = {
+    name : "Rohan",
+    age : 23,
+    location : "Hyderabad",
+    email : "jajee@google.com",
+    isLoggedIn : false,
+    loggedInDays : ["Monday", "Saturday"]
+}
+
+// Object  can be accessed in 2 ways
+// 1. using . -> dot
+console.log(jsUser.name);
+
+
+// 2. using []
+console.log(jsUser["name"]);
+
+
